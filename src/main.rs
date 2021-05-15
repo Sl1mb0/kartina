5ca1ceb062a334dbe676108017e08bda433ad172
Timothy Maloney <tmaloney@pdx.edu>
2021-05-15 15:50:09 -0700

    replaced nix::unistd with std::thread

--- src/main.rs
+++ src/main.rs
@@ -1,54 +1,37 @@
-use nannou::prelude::*;
-use nix::unistd::{fork, ForkResult};
+use std::{thread, time::Duration};
+use winit::{
+    event::*,
+    event_loop::{ControlFlow, EventLoop},
+    window::WindowBuilder,
+};
 
 fn main() {
-    match fork() {
-        Ok(ForkResult::Parent { child, .. }) => { 
-            play::play("./song/Can I Take A Picture With You.mp3").unwrap();
-        }
-        Ok(ForkResult::Child) => {
-            nannou::app(model)
-                .update(update)
-                .simple_window(view)
-                .run();
-        },
-        Err(_) => println!("Fork failed."),
-    }
-}
-
-struct Model {}
-
-fn model(_app: &App) -> Model {
-    Model {} 
-}
-
-// Handle events related to the window and update the model if necessary
-fn update(_app: &App, _model: &mut Model, _update: Update) {
-}
-
-// Draw the state of your `Model` into the given `Frame` here.
-fn view(app: &App, _model: &Model, frame: Frame) {
-    // prepare to draw
-    let draw = app.draw();
-
-    // Generate sine wave stuff based on time of app
-    let sine = app.time.sin();
-    let fastersine = (app.time * 2.0).sin();
-
-    // Get boundary of window to constrain movements of circle
-    let boundary = app.window_rect();
-
-    // Map sine wave functions to ranges between window boundaries
-    let x = map_range(sine, -1.0, 1.0, boundary.left(), boundary.right());
-    let y = map_range(fastersine, -1.0, 1.0, boundary.bottom(), boundary.top());
-
-    // color background
-    frame.clear(BLACK);
+    thread::spawn(|| {
+        // child thread
+        play::play("./song/Can I Take A Picture With You.mp3").unwrap();
+        thread::sleep(Duration::from_millis(1));
+    });
     
-    // draw a green ellipse
-    draw.ellipse().color(GREEN).x_y(x, y);
+    env_logger::init();
+    let event_loop = EventLoop::new();
+    let window = WindowBuilder::new().build(&event_loop).unwrap();
 
-    draw.to_frame(app, &frame).unwrap();
-    // shapes::drawCube(point, WHITE, frame); <- this is how I want the code to look
-    // shapes::drawTriangle(point, GREEN, frame);
+    event_loop.run(move |event, _, control_flow| match event {
+        Event::WindowEvent {
+            ref event,
+            window_id,
+        } if window_id == window.id() => match event {
+            WindowEvent::CloseRequested => *control_flow = ControlFlow::Exit,
+            WindowEvent::KeyboardInput { input, .. } => match input {
+                KeyboardInput {
+                    state: ElementState::Pressed,
+                    virtual_keycode: Some(VirtualKeyCode::Escape),
+                    ..
+                } => *control_flow = ControlFlow::Exit,
+                _ => {}
+            },
+            _ => {}
+        },
+        _ => {}
+    });
 }
