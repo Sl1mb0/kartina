a831676f92f4ede51b696c52d87d13ce00c0adc0
Timothy Maloney <tmaloney@pdx.edu>
2021-06-09 20:53:52 -0700

    Added feat: sphere self-rotation

--- src/state/mod.rs
+++ src/state/mod.rs
@@ -24,7 +24,53 @@ use winit::{
 };
 
 mod vertex;
+mod camera;
 
+/// todo!()
+struct UniformStaging {
+    camera: camera::Camera,
+    model_rotation: cgmath::Deg<f32>,
+}
+/// todo!()
+impl UniformStaging {
+    /// todo!()
+    fn new(camera: camera::Camera) -> Self {
+        Self {
+            camera,
+            model_rotation: cgmath::Deg(0.0),
+        }
+    }
+
+    /// todo!()
+    fn update_uniforms(&self, uniforms: &mut Uniforms) {
+        uniforms.view_proj = (
+            camera::OPENGL_TO_WGPU_MATRIX
+            * self.camera.build_view_projection_matrix()
+            * cgmath::Matrix4::from_angle_z(self.model_rotation)
+        ).into();
+    }
+}
+
+/// todo!()
+#[repr(C)]
+#[derive(Debug, Copy, Clone, bytemuck::Pod, bytemuck::Zeroable)]
+struct Uniforms {
+    // cgmath cannot be used with bytemuck directly;
+    // Matrix4 must be converted into 4x4 `[f32]`.
+    view_proj: [[f32; 4]; 4],
+}
+
+impl Uniforms {
+    /// todo!
+    fn new() -> Self {
+        use cgmath::SquareMatrix;
+        Self {
+            view_proj: cgmath::Matrix4::identity().into(),
+        }
+    }
+}
+
+/// todo!()
 pub struct State {
     surface : wgpu::Surface,
     device: wgpu::Device,
@@ -32,6 +78,10 @@ pub struct State {
     sc_desc: wgpu::SwapChainDescriptor,
     swap_chain: wgpu::SwapChain,
     clear_color: wgpu::Color,
+    uniforms: Uniforms,
+    uniform_staging: UniformStaging,
+    uniform_buffer: wgpu::Buffer,
+    uniform_bind_group: wgpu::BindGroup,
     render_pipeline: wgpu::RenderPipeline,
     vertex_buffer: wgpu::Buffer,
     index_buffer: wgpu::Buffer,
@@ -45,16 +95,16 @@ impl State {
     // that implements the `Future` trait.
     // Normally calling a Blocking function would block the whole thread
     //` blocked `Future`s will yield control of the thread to other `Future`s
+
+    /// todo!()
     pub async fn new(window: &Window) -> Self {
         let size = window.inner_size();
         // `instance` is a handle to the GPU
         // BackendBit::PRIMARY => Vulkan + Metal + DX12 + Browser WebGPU      
         let instance = wgpu::Instance::new(wgpu::BackendBit::PRIMARY);
-
         // `surface` is used to create swapchain
         // for more on swapchains: [swap chain](https://en.wikipedia.org/wiki/Swap_chain)
         let surface = unsafe { instance.create_surface(window) };
-
         // `adapter` is needed to create the device and queue
         // `features` field in DeviceDescriptor allows us to specify extra features
         let adapter = instance
@@ -63,7 +113,6 @@ impl State {
                 compatible_surface: Some(&surface),
                 },
             ).await.unwrap();
-
         let (device, queue) = adapter
             .request_device(
                 &wgpu::DeviceDescriptor {
@@ -75,7 +124,6 @@ impl State {
             )
             .await
             .unwrap();
-
         let sc_desc = wgpu::SwapChainDescriptor {
             usage: wgpu::TextureUsage::RENDER_ATTACHMENT,
             format: adapter.get_swap_chain_preferred_format(&surface),
@@ -83,21 +131,62 @@ impl State {
             height: size.height,
             present_mode: wgpu::PresentMode::Fifo,
         };
-
         let swap_chain = device.create_swap_chain(&surface, &sc_desc);
-
         let clear_color = wgpu::Color::WHITE;
+        let vs_module = device.create_shader_module(&wgpu::include_spirv!("./shaders/shader.vert.spv"));
+        let fs_module = device.create_shader_module(&wgpu::include_spirv!("./shaders/shader.frag.spv"));
+        let camera = camera::Camera {
+            eye: (0.0, 1.0, 2.0).into(),
+            target: (0.0, 0.0, 0.0).into(),
+            up: cgmath::Vector3::unit_y(),
+            aspect: sc_desc.width as f32 / sc_desc.height as f32,
+            fovy: 45.0,
+            znear: 0.1,
+            zfar: 100.0,
+        };
+        let mut uniforms = Uniforms::new();
+        let uniform_staging = UniformStaging::new(camera);
+        uniform_staging.update_uniforms(&mut uniforms);
 
-        let vs_module = device.create_shader_module(&wgpu::include_spirv!("shader.vert.spv"));
-        let fs_module = device.create_shader_module(&wgpu::include_spirv!("shader.frag.spv"));
-
+        let uniform_buffer = device.create_buffer_init(
+            &wgpu::util::BufferInitDescriptor {
+                label: Some("Uniform Buffer"),
+                contents: bytemuck::cast_slice(&[uniforms]),
+                usage: wgpu::BufferUsage::UNIFORM | wgpu::BufferUsage::COPY_DST,
+            }
+        );
+        let uniform_bind_group_layout = device.create_bind_group_layout(
+            &wgpu::BindGroupLayoutDescriptor {
+                entries: &[
+                    wgpu::BindGroupLayoutEntry {
+                        binding: 0,
+                        visibility: wgpu::ShaderStage::VERTEX,
+                        ty: wgpu::BindingType::Buffer {
+                            ty: wgpu::BufferBindingType::Uniform,
+                            has_dynamic_offset: false,
+                            min_binding_size: None,
+                        },
+                        count: None,
+                    }
+                ],
+                label: Some("uniform_bind_group_layout")
+        });
+        let uniform_bind_group = device.create_bind_group(&wgpu::BindGroupDescriptor {
+            layout: &uniform_bind_group_layout,
+            entries: &[
+                wgpu::BindGroupEntry {
+                    binding: 0,
+                    resource: uniform_buffer.as_entire_binding(),
+                }
+            ],
+            label: Some("uniform_bind_group"),
+        });
         let render_pipeline_layout = 
             device.create_pipeline_layout(&wgpu::PipelineLayoutDescriptor {
                 label: Some("Render Pipeline Layout"),
-                bind_group_layouts: &[],
+                bind_group_layouts: &[&uniform_bind_group_layout],
                 push_constant_ranges: &[]
             });
-
         let render_pipeline = device.create_render_pipeline(&wgpu::RenderPipelineDescriptor {
             label: Some("Render Pipeline!"),
             layout: Some(&render_pipeline_layout),
@@ -130,8 +219,7 @@ impl State {
                 mask: !0,
                 alpha_to_coverage_enabled: false
             },
-        });
-        
+        });        
         let vbo = vertex::Vertex::sphere_vertices(1.0);
         let vertex_buffer = device.create_buffer_init(
             &wgpu::util::BufferInitDescriptor {
@@ -139,8 +227,7 @@ impl State {
                 contents: bytemuck::cast_slice(&vbo),
                 usage: wgpu::BufferUsage::VERTEX,
             }
-        );
-        
+        );       
         let ibo = vertex::Vertex::sphere_indices();
         let index_buffer = device.create_buffer_init(
             &wgpu::util::BufferInitDescriptor {
@@ -149,9 +236,7 @@ impl State {
                 usage: wgpu::BufferUsage::INDEX,
             }
         );
-
         let num_indices = ibo.len() as u32;
-
         Self {
             surface,
             device,
@@ -159,6 +244,10 @@ impl State {
             sc_desc,
             swap_chain,
             clear_color,
+            uniforms,
+            uniform_staging,
+            uniform_buffer,
+            uniform_bind_group,
             render_pipeline,
             vertex_buffer,
             index_buffer,
@@ -167,6 +256,7 @@ impl State {
         }
     }
 
+    /// todo!()
     pub fn resize(&mut self, new_size: winit::dpi::PhysicalSize<u32>) {
         self.size = new_size;
         self.sc_desc.width = new_size.width;
@@ -174,6 +264,7 @@ impl State {
         self.swap_chain = self.device.create_swap_chain(&self.surface, &self.sc_desc);
     }
 
+    /// todo!()
     pub fn input(&mut self, frame: &Frame) -> bool {
         let mut vertices = vertex::Vertex::sphere_vertices(1.0);
         for vertex in &mut vertices {
@@ -194,43 +285,45 @@ impl State {
         );
         true
     }
-
+    
+    /// todo!()
     pub fn update(&mut self) {
-        // todo!()
+        self.uniform_staging.model_rotation += cgmath::Deg(2.0);
+        self.uniform_staging.update_uniforms(&mut self.uniforms);
+        self.queue.write_buffer(
+            &self.uniform_buffer,
+            0,
+            bytemuck::cast_slice(&[self.uniforms]),
+        );
     }
 
+    /// todo!()
     pub fn render(&mut self) -> Result<(), wgpu::SwapChainError> {
         let frame = self
             .swap_chain
             .get_current_frame()?
             .output;
-
         let mut encoder = self
             .device
             .create_command_encoder(&wgpu::CommandEncoderDescriptor { 
                 label: Some("Render Encoder"),
             });
-
         // `encoder.begin_render_pass()` borrows `encoder` mutably 
         // therefore, `encoder.finish()` cannot be called
         // until the mutable borrow is released by `encoder.begin_render_pass()
-        let mut render_pass = encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
-            
+        let mut render_pass = encoder.begin_render_pass(&wgpu::RenderPassDescriptor {      
             // RenderPassDescriptor has three fields: `label`, `color_attachment`, and `depth_stencil_attachment`
             // color_attachments describes where color will be drawn to
             label: Some("Render Pass"),
-            color_attachments: &[wgpu::RenderPassColorAttachmentDescriptor {
-                
+            color_attachments: &[wgpu::RenderPassColorAttachmentDescriptor {              
                 // `attachment` informs wgpu what textures to save the colors to
                 // in this case, we have specified frame.view 
                 // (that was created with swap_chain.get_current_frame())
                 // esentially any colors drawn to this attachment will be drawn to the screen              
-                attachment: &frame.view,
-                
+                attachment: &frame.view,              
                 // `resolve_target` is the texture that will receive the resolved output
                 // This will be the same as `attachment` unless multisampling is enabled
                 resolve_target: None,
-
                 // `ops` takes a `wgpu::Operations` object. this tells wgpu
                 // what to do with the colors on the screen (specified by frame.view) 
                 // `load` tells wgpu how to handle colors stored from the previous frame
@@ -243,16 +336,15 @@ impl State {
         });
 
         render_pass.set_pipeline(&self.render_pipeline);
+        render_pass.set_bind_group(0, &self.uniform_bind_group, &[]);
         render_pass.set_vertex_buffer(0, self.vertex_buffer.slice(..));
         render_pass.set_index_buffer(self.index_buffer.slice(..), wgpu::IndexFormat::Uint16);
         render_pass.draw_indexed(0..self.num_indices, 0, 0..1);
 
         // release the mutable borrow
         // so that `finish` may be called by encoder.
-
         drop(render_pass);
         self.queue.submit(iter::once(encoder.finish()));
-
         Ok(())
     }
 }
